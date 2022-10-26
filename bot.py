2cd8dec5cb1d989ff70e09854c08479df771922c
Guy Mann <guydmann@gmail.com>
2022-10-26 14:02:28 -0400

    made improvements to the bot so that it will list the options instead of having to make separate calls to list the algorithms

--- bot.py
+++ bot.py
@@ -1,15 +1,14 @@
 import asyncio
 import os
 from dotenv import load_dotenv
-import discord 
-# from discord.ext import commands, tasks
+import discord
 import glob
 import functools
 import typing
 import logging
 import subprocess
 import sys
-from discord.commands import Option
+from discord import option
 import time
 
 logging.basicConfig(level=logging.INFO)
@@ -22,8 +21,9 @@ bot = discord.Bot(debug_guilds=[987960420851666965], intents=intents) # specify 
 
 # discord_key = os.environ["discord_key"]
 
-# async def is_channel_fractal(ctx):
-#     return ctx.channel.id == 1001295615508086924
+
+def is_channel_fractal(ctx):
+    return int(ctx.channel.id) == int(os.environ["channel_id"])
 
 
 def create_fractal_animation(animation, width, height, color_algorithm, fps):
@@ -74,56 +74,84 @@ async def run_blocking_del(blocking_func: typing.Callable, *args, **kwargs) -> t
 
 
 @bot.command(description='Create Fractal Animation')
-async def fractal_animation(ctx, animation: Option(str, "use /animation_list to get a list of options or see pinned messages", required = True, default = 'random_phoenix_julia'),
-    width: Option(str, "Default is 512, experiment with this", required = True, default = '512'), height: Option(str, "Default is 512, experiment with this", required = True, default = '512'), color_algorithm: Option(str, "simple, black_and_white, hue_range, hue_cyclic", required = True, default = 'hue_range'),
-    fps: Option(str, "Default is 12, experiment with this", required = True, default = '12')):
+@option(
+    "animation",
+    description="the animation algorithm to use",
+    required=True,
+    default="random_phoenix_julia",
+    choices=["first_hue_rotation", "second_hue_rotation", "hue_cycle", "random_julia", "random_cubic_julia", "random_phoenix_julia", "random_quartic_julia", "random_walk_julia"]
+)
+@option("width", description="The width of the image. Default is 512", required=True, default=512)
+@option("height", description="The height of the image. Default is 512", required=True, default=512)
+@option(
+    "color_algorithm",
+    description="the coloring algorithm to use",
+    required=True,
+    default="simple",
+    choices=["simple", "black_and_white", "hue_range", "hue_cyclic"])
+@option(
+    "fps",
+    description="The number of frames per second in the animation. Default is 12",
+    required=True,
+    default=12)
+async def fractal_animation(
+        ctx,
+        animation: str,
+        width: int,
+        height: int,
+        color_algorithm: str,
+        fps: int
+):
     role = discord.utils.get(ctx.guild.roles, name="AiFREN")
-    if role in ctx.author.roles and ctx.channel.id == 1001295615508086924:
+    if role in ctx.author.roles and is_channel_fractal(ctx):
         userid = ctx.author.id
         await ctx.respond(f"<@{userid}> Starting...", ephemeral=True)
         name = await run_blocking_animation(create_fractal_animation, animation, width, height, color_algorithm, fps)
-        list_of_files = glob.glob('images/' + animation + '/*') # * means all if need specific format then *.csv
+        list_of_files = glob.glob(f'images/{animation}/*')     # "*" means all if need specific format then *.csv
         my_filename = max(list_of_files, key=os.path.getctime)
         await ctx.respond(content=f"<@{userid}> **animation: {animation}, width: {width}, height: {height}, color_algorith: {color_algorithm}, fps: {fps}**", file=discord.File(my_filename))
-        list_of_files = glob.glob('images/' + animation + '/*') # * means all if need specific format then *.csv
+        list_of_files = glob.glob(f'images/{animation}/*') # * means all if need specific format then *.csv
         await run_blocking_del(del_files, list_of_files)
     else:
-        await ctx.respond(f"**You can only use this command if you have AiFRENS role and are in the fractal-art room**", ephemeral=True)
+        await ctx.respond(
+            "**You can only use this command if you have AiFRENS role and are in the fractal-art room**",
+            ephemeral=True)
 
 
-@bot.command(description='Create Fractal Art')
-async def fractal_art(ctx, algo: Option(str, "use /algo_list to get a list of options or see pinned messages", required = True, default = 'mandelbrot'),
-    width: Option(str, "Default is 512, experiment with this", required = True, default = '512'), height: Option(str, "Default is 512, experiment with this", required = True, default = '512'), color_algorithm: Option(str, "simple, black_and_white, hue_range, hue_cyclic", required = True, default = 'hue_range'),
+@bot.slash_command(description='Create Fractal Art')
+@option(
+    "algo",
+    description="the algorithm to use",
+    required=True,
+    choices=["mandelbrot", "julia", "burning_ship", "star", "newton", "phoenix_mandelbrot", "phoenix_julia", "cubic_mandelbrot", "quartic_mandelbrot", "cubic_julia", "experimental_cubic_julia", "quartic_julia", "buddhabrot", "buddhabrot_julia"]
+)
+@option("width", description="The width of the image. Default is 512", required=True, default=512)
+@option("height", description="The height of the image. Default is 512", required=True, default=512)
+@option(
+    "color_algorithm",
+    description="the coloring algorithm to use",
+    required=True,
+    default="simple",
+    choices=["simple", "black_and_white", "hue_range", "hue_cyclic"])
+async def fractal_art(
+        ctx: discord.ApplicationContext,
+        algo: str,
+        width: int,
+        height: int,
+        color_algorithm: str,
     ):
     role = discord.utils.get(ctx.guild.roles, name="AiFREN")
-    if role in ctx.author.roles and ctx.channel.id == 1001295615508086924:
+    if role in ctx.author.roles and is_channel_fractal(ctx):
         userid = ctx.author.id
         await ctx.respond(f"<@{userid}> Starting...", ephemeral=True)
         name = await run_blocking_art(create_fractal_art, algo, width, height, color_algorithm)
-        list_of_files = glob.glob('images/*') # * means all if need specific format then *.csv
+        list_of_files = glob.glob('images/*')       # "*" means all if need specific format then *.csv
         my_filename = max(list_of_files, key=os.path.getctime)
         await ctx.respond(content=f"<@{userid}> **algorithm: {algo}, width: {width}, height: {height}, color_algorith: {color_algorithm}**", file=discord.File(my_filename))
-        # list_of_files = glob.glob('images/*') # * means all if need specific format then *.csv
-        # await run_blocking_del(del_files, list_of_files)
     else:
-        await ctx.respond(f"**You can only use this command if you have AiFRENS role and are in the fractal-art room**", ephemeral=True)
+        await ctx.respond(
+            "**You can only use this command if you have AiFRENS role and are in the fractal-art room**",
+            ephemeral=True)
 
-
-@bot.command(description='Show list of fractal algorithms')
-async def algo_list(ctx):
-    # role = discord.utils.get(ctx.guild.roles, name="AiFREN")
-    if ctx.channel.id == 1001295615508086924:
-        await ctx.respond("mandelbrot, julia, burning_ship, star, newton, phoenix_mandelbrot, phoenix_julia, cubic_mandelbrot, quartic_mandelbrot, cubic_julia, experimental_cubic_julia, quartic_julia, buddhabrot, buddhabrot_julia")
-    else:
-        await ctx.respond(f"**You can only use this command if you are in the fractal-art room**", ephemeral=True)
-
-
-@bot.command(description='Show list of fractal animations')
-async def animation_list(ctx):
-    role = discord.utils.get(ctx.guild.roles, name="AiFREN")
-    if role in ctx.author.roles and ctx.channel.id == 1001295615508086924:
-        await ctx.respond("first_hue_rotation, second_hue_rotation, hue_cycle, random_julia, random_cubic_julia, random_phoenix_julia, random_quartic_julia, random_walk_julia")
-    else:
-        await ctx.respond(f"**You can only use this command if you have AiFRENS role and are in the fractal-art room**", ephemeral=True)
 
 bot.run(os.environ["discord_key"])
