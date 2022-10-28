d299cedbb00fd0def82c9ac0d9d895a6bf7bcc92
Guy Mann <guydmann@gmail.com>
2022-10-28 10:44:18 -0400

    improved the bot code for the fractal art command to give it all the available options

--- bot.py
+++ bot.py
@@ -11,7 +11,6 @@ import subprocess
 import sys
 from discord import option
 import time
-from generate_fractal_image import main as generate_fractal, add_arguments_to_parser
 
 logging.basicConfig(level=logging.INFO)
 
@@ -20,8 +19,6 @@ load_dotenv()
 intents = discord.Intents.all()
 
 bot = discord.Bot(debug_guilds=[987960420851666965], intents=intents) # specify the guild IDs in debug_guilds
-
-# discord_key = os.environ["discord_key"]
 
 
 def is_channel_fractal(ctx):
@@ -34,25 +31,14 @@ def create_fractal_animation(animation, width, height, color_algorithm, fps):
     return
 
 
-def create_fractal_art(algo, width, height, color_algorithm):
+def create_fractal_art(fractal_args):
     name = "images/{time}".format(time=time.time())
-    #subprocess.run([sys.executable, 'generate_fractal_image.py', '-a', algo, '-W', width, '-H', height, '-c', color_algorithm, '-f', name])
-    parser = argparse.ArgumentParser(add_help=True)
-    parser = add_arguments_to_parser(parser)
-    argument_list = [
-        '--fractal_algorithm', algo,
-        '--width', str(width),
-        '--height', str(height),
-        '--color_algorithm', color_algorithm,
-        '--filename', name,
-        '--real_constant', '-0.844',
-        '--imaginary_constant', '0.2',
-    ] + "--viewport_left -1 --viewport_right 1 --viewport_top 1 --viewport_bottom -1 ".split()
-
+    argument_list = [sys.executable, 'generate_fractal_image.py', '--filename', name]
+    for key, value in fractal_args.items():
+        argument_list.append(f"--{key}")
+        argument_list.append(str(value))
     print(argument_list)
-    #-cr -0.844 -ci 0.2
-    arguments = parser.parse_args(argument_list)
-    generate_fractal(arguments)
+    subprocess.run(argument_list)
     return f"{name}.png"
 
 
@@ -83,22 +69,19 @@ async def run_blocking_del(blocking_func: typing.Callable, *args, **kwargs) -> t
 @option(
     "animation",
     description="the animation algorithm to use",
-    required=True,
     default="random_phoenix_julia",
     choices=["first_hue_rotation", "second_hue_rotation", "hue_cycle", "random_julia", "random_cubic_julia", "random_phoenix_julia", "random_quartic_julia", "random_walk_julia"]
 )
-@option("width", description="The width of the image. Default is 512", required=True, default=512)
-@option("height", description="The height of the image. Default is 512", required=True, default=512)
+@option("width", description="The width of the image. Default is 512", default=512)
+@option("height", description="The height of the image. Default is 512", default=512)
 @option(
     "color_algorithm",
     description="the coloring algorithm to use",
-    required=True,
     default="simple",
     choices=["simple", "black_and_white", "hue_range", "hue_cyclic"])
 @option(
     "fps",
     description="The number of frames per second in the animation. Default is 12",
-    required=True,
     default=12)
 async def fractal_animation(
         ctx,
@@ -126,32 +109,90 @@ async def fractal_animation(
 
 @bot.slash_command(description='Create Fractal Art')
 @option(
-    "algo",
+    "fractal_algorithm",
     description="the algorithm to use",
-    required=True,
+    default="mandelbrot",
     choices=["mandelbrot", "julia", "burning_ship", "star", "newton", "phoenix_mandelbrot", "phoenix_julia", "cubic_mandelbrot", "quartic_mandelbrot", "cubic_julia", "experimental_cubic_julia", "quartic_julia", "buddhabrot", "buddhabrot_julia"]
 )
-@option("width", description="The width of the image. Default is 512", required=True, default=512)
-@option("height", description="The height of the image. Default is 512", required=True, default=512)
+@option("width", description="The width of the image. Default is 512", default=512)
+@option("height", description="The height of the image. Default is 512", default=512)
 @option(
     "color_algorithm",
     description="the coloring algorithm to use",
-    required=True,
     default="simple",
     choices=["simple", "black_and_white", "hue_range", "hue_cyclic"])
+@option(
+    "image_filtering",
+    default="none",
+    description="applies specific filters to the image for different visual effects",
+    choices=['none', 'basic', 'glow_takeover', 'additive_glow', 'blur'])
+@option("viewport_left", description="coordinate of left edge of view port", required=False)
+@option("viewport_right", description="coordinate of right edge of view port", required=False)
+@option("viewport_top", description="coordinate of top edge of view port", required=False)
+@option("viewport_bottom", description="coordinate of bottom edge of view port", required=False)
+@option("real_constant", description="the real portion of the constant in Julia type calculations.", required=False)
+@option("imaginary_constant", description="the imaginary portion of the constant in Julia type calculations.", required=False)
+@option("precision", description="The precision to be used in the fractal calculations", required=False)
+@option("hue_start_degree", description="use to define hue range for hue_range amd hue_cyclic color algorithms", required=False)
+@option("hue_end_degree", description="use to define hue range for hue_range color algorithm", required=False)
+@option("hue_step_shift", description="use to define hue steps to take in the hue_cyclic color algorithm", required=False)
+@option("color_count", description="use to define the number of colors to cycle through in the cyclic color algorithms", required=False)
 async def fractal_art(
         ctx: discord.ApplicationContext,
-        algo: str,
+        fractal_algorithm: str,
         width: int,
         height: int,
         color_algorithm: str,
-    ):
+        image_filtering: str,
+        viewport_left: float,
+        viewport_right: float,
+        viewport_top: float,
+        viewport_bottom: float,
+        real_constant: float,
+        imaginary_constant: float,
+        precision: int,
+        hue_start_degree: int,
+        hue_end_degree: int,
+        hue_step_shift: int,
+        color_count: int,
+):
     role = discord.utils.get(ctx.guild.roles, name="AiFREN")
     if role in ctx.author.roles and is_channel_fractal(ctx):
         userid = ctx.author.id
         await ctx.respond(f"<@{userid}> Starting...", ephemeral=True)
-        name = await run_blocking_art(create_fractal_art, algo, width, height, color_algorithm)
-        await ctx.respond(content=f"<@{userid}> **algorithm: {algo}, width: {width}, height: {height}, color_algorith: {color_algorithm}**", file=discord.File(name))
+        fractal_args = {
+            "fractal_algorithm": fractal_algorithm,
+            "color_algorithm": color_algorithm,
+            "width": width,
+            "height": height,
+        }
+        if viewport_left:
+            fractal_args["viewport_left"] = viewport_left
+        if viewport_right:
+            fractal_args["viewport_right"] = viewport_right
+        if viewport_top:
+            fractal_args["viewport_top"] = viewport_top
+        if viewport_bottom:
+            fractal_args["viewport_bottom"] = viewport_bottom
+        if real_constant:
+            fractal_args["real_constant"] = real_constant
+        if imaginary_constant:
+            fractal_args["imaginary_constant"] = imaginary_constant
+        if precision:
+            fractal_args["precision"] = precision
+        if hue_start_degree:
+            fractal_args["hue_start_degree"] = hue_start_degree
+        if hue_end_degree:
+            fractal_args["hue_end_degree"] = hue_end_degree
+        if hue_step_shift:
+            fractal_args["hue_step_shift"] = hue_step_shift
+        if color_count:
+            fractal_args["color_count"] = color_count
+        if image_filtering:
+            fractal_args["image_filtering"] = image_filtering
+        print(fractal_args)
+        name = await run_blocking_art(create_fractal_art, fractal_args)
+        await ctx.respond(content=f"<@{userid}> **algorithm: {fractal_algorithm}, width: {width}, height: {height}, color_algorith: {color_algorithm}**", file=discord.File(name))
     else:
         await ctx.respond(
             "**You can only use this command if you have AiFRENS role and are in the fractal-art room**",
